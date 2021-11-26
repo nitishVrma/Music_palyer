955b2431518daa48c197cdbe9cf5e850bfd19a8c
Nitish <ozeus459@gmail.com>
2021-11-26 06:40:24 +0530

    initial functionality

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,17 @@
-let action = document.getElementsByClassName('action-btn')
+let playBtn = document.getElementById('play')
+let isplaying = false;
+const music = document.querySelector('audio');
+let icon = document.getElementsByClassName('fas')[0]
+
+playBtn.addEventListener('click', () => {
+    if(isplaying){
+       music.pause()
+       icon.classList.replace("fa-pause","fa-play")
+    }else{
+        music.play()
+        icon.classList.replace("fa-play","fa-pause")
+    }
+    isplaying = !isplaying;
+    
+    
+  })
